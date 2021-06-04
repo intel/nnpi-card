6287a5e7b15c319c0fea0264c4e9cd168060e9b1
coh_sw <coh_sw@intel.com>
2021-06-04 17:56:54 +0300

    Dev changes [3ebf39122724b29df724fbe99ece084bf40a38be]

--- ice_driver/driver/sph/sph_device_regs.h
+++ ice_driver/driver/sph/sph_device_regs.h
@@ -1,5 +1,5 @@
 /********************************************
- * Copyright (C) 2019-2020 Intel Corporation
+ * Copyright (C) 2019-2021 Intel Corporation
  *
  * SPDX-License-Identifier: GPL-2.0-or-later
  ********************************************/
@@ -99,6 +99,8 @@ struct config {
 	uint32_t ice_dso_filter_mask_low_ch1_reg_offset;
 	uint32_t ice_dso_filter_mask_high_ch1_reg_offset;
 	uint32_t ice_dso_filter_inv_ch1_reg_offset;
+	uint32_t ice_dso_cfg_dtf_src_cfg_src_en_mask;
+	uint32_t ice_dso_cfg_dtf_src_cfg_ch_en_mask;
 	uint32_t ice_dbg_cbbid_base;
 	uint32_t ice_dbg_cbbid_cfg_offset;
 	uint32_t ice_prog_cores_ctrl_offset;
